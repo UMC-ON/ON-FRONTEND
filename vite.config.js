88cdfcbe317bddc6c1d0090dec49658d1da8dde3
Seojung <sjhan0814@gmail.com>
2024-09-06 15:06:50 +0900

    vercel port 수정

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
+import vercel from 'vite-plugin-vercel';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   server: {
-    host: 'localhost',
-    port: 3000,
+    port: 443,
   },
+  plugins: [react(), vercel()],
 });
