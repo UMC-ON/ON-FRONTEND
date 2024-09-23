a409ad708169ce66b0a2599ef3ddb21673ce0ffc
Seojung <sjhan0814@gmail.com>
2024-09-23 17:08:53 +0900

    fix. 디자이너 리팩토링에 맞춰 style 수정

--- src/components/Notification/SingleNotification.jsx
+++ src/components/Notification/SingleNotification.jsx
@@ -1,20 +1,11 @@
 import * as s from './SingleNotificationStyled';
-import notification_circle from '../../assets/images/notification_circle.svg';
 
-const SingleNotification = () => {
+const SingleNotification = ({ title, content, alertType, alertConnectId }) => {
   return (
     <s.NotificationContainer>
-      <s.NotificationTitle>[정보글]</s.NotificationTitle>
-      <s.NotificationTitle>
-        내 동행글에 새로운 동행 신청 메시지가 왔어요
-      </s.NotificationTitle>
-      <s.NotificationText>
-        그러면 6개월 전부터 시도해 보면 괜찮을까요?
-      </s.NotificationText>
-      <s.NotificationCircle
-        src={notification_circle}
-        alt="Notification Circle"
-      />
+      <s.NotificationTitle>{title}</s.NotificationTitle>
+      <s.NotificationText>{content}</s.NotificationText>
+      <s.NotificationCircle />
     </s.NotificationContainer>
   );
 };
