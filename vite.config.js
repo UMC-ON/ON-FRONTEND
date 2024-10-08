468d5b74b7694dbc2699e3a25f47cc508b32d7b5
한서정 / Seojung Han <144990897+xseojungx@users.noreply.github.com>
2024-10-08 16:41:09 +0900

    포트 번호 3000번으로 수정

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
-import vercel from 'vite-plugin-vercel';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
-    port: 443,
+    host: 'localhost',
+    port: 3000,
   },
-  plugins: [react(), vercel()],
 });
