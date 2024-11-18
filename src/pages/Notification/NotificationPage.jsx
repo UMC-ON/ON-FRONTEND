f5eae6aaa92299b8b8a92e095e149d7b7a250e66
sjhan0814 <sjhan0814@gmail.com>
2024-11-18 17:03:38 +0900

    notification 인피니트 스크롤

--- src/pages/Notification/NotificationPage.jsx
+++ src/pages/Notification/NotificationPage.jsx
@@ -7,62 +7,65 @@ import { useEffect, useState } from 'react';
 import { GET_ALERT_LIST } from '../../api/urls';
 import Loading from '../../components/Loading/Loading';
 import SingleNotification from '../../components/Notification/SingleNotification';
+import { useInfiniteQuery } from 'react-query';
 
 const Notification = () => {
   const [notification, setNotification] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    const fetchNotification = async () => {
-      setIsLoading(true);
-      try {
-        const response = await getData(
-          GET_ALERT_LIST,
-          {
-            Authorization: `Bearer ${localStorage.getItem('AToken')}`,
-          },
-          { sort: 'createdAt%2Cdesc' },
-        );
+  const fetchNotifications = async ({ pageParam = 0 }) => {
+    const response = await getData(
+      GET_ALERT_LIST,
+      {
+        Authorization: `Bearer ${localStorage.getItem('AToken')}`,
+      },
+      { page: pageParam, size: 20, sort: 'createdAt%2Cdesc' },
+    );
+    return response.data;
+  };
 
-        // 응답 데이터의 구조를 확인하고 유효성을 검사
-        console.log('Received data:', response.data); // 전체 응답 데이터 확인
-        setNotification(response.data); // 상태 업데이트
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    fetchNotification();
-  }, []);
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useInfiniteQuery('notifications', fetchNotifications, {
+      staleTime: 0,
+      getNextPageParam: (lastPage, allPages) => {
+        if (lastPage.length < 20) return undefined; // 데이터가 없으면 멈춤
+        return allPages.length; // 다음 페이지 번호 반환
+      },
+    });
+
+  const handleScroll = (e) => {
+    if (
+      e.target.scrollHeight - e.target.scrollTop <=
+        e.target.clientHeight + 50 &&
+      hasNextPage
+    ) {
+      fetchNextPage();
+    }
+  };
 
   if (isLoading) {
     return <Loading />;
   }
   return (
-    <s.NotificationLayout>
+    <s.NotificationLayout
+      style={{ overflowY: 'auto', height: '100vh' }}
+      onScroll={handleScroll}
+    >
       <PageHeader pageName="알림" />
-      {notification && notification.length > 0 ? (
-        <>
-          <s.NotificationWrapper>
-            {notification.map((data) => (
-              <SingleNotification
-                key={data.alertId}
-                id={data.alertId}
-                title={data.title}
-                content={data.content}
-                alertType={data.alertType}
-                alertConnectId={data.alertConnectId}
-                read={data.read}
-              />
-            ))}
-          </s.NotificationWrapper>
-          <s.NotificationEnd>알람 내역의 마지막입니다.</s.NotificationEnd>
-          <s.Background />
-        </>
-      ) : (
-        <NoContent content="알림 내역" />
+      {data?.pages.map((page, i) =>
+        page.map((data) => (
+          <SingleNotification
+            key={data.alertId}
+            id={data.alertId}
+            title={data.title}
+            content={data.content}
+            alertType={data.alertType}
+            alertConnectId={data.alertConnectId}
+            read={data.read}
+          />
+        )),
       )}
+      {isFetchingNextPage && <Loading />}
     </s.NotificationLayout>
   );
 };
