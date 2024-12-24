dbbfab59078be5af275c85d9a4543c49ea7678b3
sjhan0814 <sjhan0814@gmail.com>
2024-12-24 17:05:13 +0900

    firebase 수정

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,19 +5,27 @@ importScripts(
   'https://www.gstatic.com/firebasejs/10.5.0/firebase-messaging-compat.js',
 );
 
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] installed', e);
+const firebaseConfig = {
+  apiKey: 'AIzaSyAA06_MAwUKfVwtAnih_q0NSnIM0G8Op0Q',
+  authDomain: 'oncommunity-f3dad.firebaseapp.com',
+  projectId: 'oncommunity-f3dad',
+  storageBucket: 'oncommunity-f3dad.appspot.com',
+  messagingSenderId: '127863478997',
+  appId: '1:127863478997:web:f922de330e110844e2605c',
+  measurementId: 'G-CHKYD0GWJ7',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+self.addEventListener('install', function (e) {
+  console.log('fcm sw install..', e);
   self.skipWaiting();
 });
 
-// activate event
-self.addEventListener('activate', (e) => {
-  console.log('[Service Worker] actived', e);
-});
-
-// fetch event
-self.addEventListener('fetch', (e) => {
-  console.log('[Service Worker] fetched resource ' + e.request.url);
+self.addEventListener('activate', function (e) {
+  console.log('fcm sw activate..', e);
 });
 
 self.addEventListener('push', function (e) {
@@ -37,27 +45,6 @@ self.addEventListener('push', function (e) {
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
-// self.addEventListener('notificationclick', function (event) {
-//   console.log('notification click');
-//   const url = '/';
-//   event.notification.close();
-//   event.waitUntil(clients.openWindow(url));
-// });
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyAA06_MAwUKfVwtAnih_q0NSnIM0G8Op0Q',
-  authDomain: 'oncommunity-f3dad.firebaseapp.com',
-  projectId: 'oncommunity-f3dad',
-  storageBucket: 'oncommunity-f3dad.appspot.com',
-  messagingSenderId: '127863478997',
-  appId: '1:127863478997:web:f922de330e110844e2605c',
-  measurementId: 'G-CHKYD0GWJ7',
-};
-
-firebase.initializeApp(firebaseConfig);
-
-const messaging = firebase.messaging();
-
 messaging.onBackgroundMessage((payload) => {
   const notificationTitle = payload.title;
   const notificationOptions = {
@@ -67,11 +54,9 @@ messaging.onBackgroundMessage((payload) => {
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
-// self.addEventListener('install', function (e) {
-//   console.log('fcm sw install..');
-//   self.skipWaiting();
-// });
-
-// self.addEventListener('activate', function (e) {
-//   console.log('fcm sw activate..');
+// self.addEventListener('notificationclick', function (event) {
+//   console.log('notification click');
+//   const url = '/';
+//   event.notification.close();
+//   event.waitUntil(clients.openWindow(url));
 // });
