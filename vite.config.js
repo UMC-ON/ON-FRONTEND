507b5cf850f6b6186c840add38b68b73e1f34e43
ruiiary <guidews39@gmail.com>
2025-01-02 23:21:10 +0900

    chore: main 배포 환경으로 변경

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite';
-//import vercel from 'vite-plugin-vercel';
+import vercel from 'vite-plugin-vercel';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   server: {
-    //port: 443,
-    port: 3000,
-    host: 'localhost',
+    port: 443,
   },
-  //plugins: [react(), vercel()],
-  plugins: [react()],
+  plugins: [react(), vercel()],
 });
