14acb75ea0c4fe4d6648f49f274c750d4aa033d2
ruiiary <guidews39@gmail.com>
2025-01-02 10:15:54 +0900

    chore: 포트번호 추가

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 443,
+    host: 'localhost',
+    port: 3000,
   },
   //plugins: [react(), vercel()],
 });
