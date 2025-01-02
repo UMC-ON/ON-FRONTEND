0f64dca9ee89f0e0cb41622af3e51a13f8b51eb0
ruiiary <guidews39@gmail.com>
2025-01-02 23:01:04 +0900

    fix: ios 빌드 시 스크롤 안 되는 문제 fix test

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
-import vercel from 'vite-plugin-vercel';
+//import vercel from 'vite-plugin-vercel';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   server: {
-    port: 443,
+    //port: 443,
+    port: 3000,
+    host: 'localhost',
   },
-  plugins: [react(), vercel()],
+  //plugins: [react(), vercel()],
+  plugins: [react()],
 });
