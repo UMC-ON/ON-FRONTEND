a2cfebe8f73f00cca5f659e6a3d74e9b7e52c075
sjhan0814 <sjhan0814@gmail.com>
2025-01-04 17:32:28 +0900

    s

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-//import vercel from 'vite-plugin-vercel';
+import vercel from 'vite-plugin-vercel';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
